39ea0a22243a4d21299fc35079a7e80b2633c012
Gab20031995 <luizclzm@gmail.com>
2021-03-07 21:51:02 +0000

    Subo ejercio simple counter

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,26 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import PropTypes from "prop-types";
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bigCounter">
+			<div className="clock">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="six"> {props.nSix % 10} </div>
+			<div className="five"> {props.nFive % 10} </div>
+			<div className="four"> {props.nFour % 10} </div>
+			<div className="three"> {props.nThree % 10} </div>
+			<div className="two"> {props.nTwo % 10} </div>
+			<div className="one"> {props.nOne % 10} </div>
 		</div>
 	);
 }
+Home.propTypes = {
+	nSix: PropTypes.number,
+	nFive: PropTypes.number,
+	nFour: PropTypes.number,
+	nThree: PropTypes.number,
+	nTwo: PropTypes.number,
+	nOne: PropTypes.number
+};
